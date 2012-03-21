90d10c00f7615eb3b22ab702b9f3fc1eeddf3f36
Jimmy Hillis <ppjim3@gmail.com>
2012-03-21 22:11:47 +0800

    Initial repo with readme description and start of JS code

---
+++ jquery.inputToggle.js
@@ -0,0 +1,24 @@
+/*
+// Simple jQuery plugin that remembers the initial state
+// of the value (on page load) and reverts back to it when
+// a user blurs and doesn't have any content within the form
+*/
+
+(function( $ ) {
+  $.fn.inputToggle = function() {
+		return this.each(function() {
+			// Set the initial value to be stored in the elements data-
+			$(this).attr('data-initialvalue', $(this).val());
+			// On focus if the default is set remove it
+			$(this).bind('focus', function() {
+				if($(this).val() === $(this).attr('data'))
+					$(this).val('');
+			});
+			// On focus if there is no value provided, set it as default
+			$(this).bind('blur', function() {
+				if($(this).val() === /\s/i)
+					$(this).val($(this).attr('data'));
+			});
+		});
+	};
+})( jQuery );
