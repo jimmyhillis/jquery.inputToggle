b17e08ef1b093736d1f543ba3bcf4eb5a9d2fe75
Jimmy Hillis <jimmy.hillis@me.com>
2012-06-20 08:40:18 +0800

    Added support for HTML5 placeholder and updated the example to reflect

--- jquery.inputToggle.js
+++ jquery.inputToggle.js
@@ -4,31 +4,51 @@
 // a user blurs and doesn't have any content within the form
 */
 (function( $ ) {
+
+	// Private methods
+	var placeholderSupport
+		, hideDefault
+		, showDefault;
+
+	// Check for HTML5 form "placeholder" support, a better
+	// solution for the whole problem
+	placeholderSupport = function() {
+		var input_element = document.createElement('input');
+		return ('placeholder' in input_element);
+	}
+
 	// If the current value matches the default, remove it.
-	var hidedefault = function() { 
+	hideDefault = function() { 
 		if($(this).val() === $(this).attr('data-initialvalue')) {
 			$(this).val('');
 		}
 	}
 	// If the current input value is empty (space)
 	// then put the default back in
-	var showdefault = function() {
+	showDefault = function() {
 		if(/^(\s)*$/i.test($(this).val())) {
 			$(this).val($(this).attr('data-initialvalue'));
 		}
 	}
+
 	// Create inputToggle public space and allow for
 	// options. Run initial functionality to set the 
 	// default values and bind listeners to events.
   $.fn.inputToggle = function(options) {
   	options = options || {};
 		return this.each(function() {
-			// Set the initial value to be stored in the elements data-
-			$(this).attr('data-initialvalue', $(this).val());
-			// On focus if the default is set remove it
-			$(this).bind('focus', hidedefault);
-			// On focus if there is no value provided, set it as default
-			$(this).bind('blur', showdefault);
+			if (placeholderSupport()) {
+				$(this).attr('placeholder', $(this).val());
+				$(this).val('');
+			} 
+			else {
+				// Set the initial value to be stored in the elements data-
+				$(this).attr('data-initialvalue', $(this).val());
+				// On focus if the default is set remove it
+				$(this).bind('focus', hideDefault);
+				// On focus if there is no value provided, set it as default
+				$(this).bind('blur', showDefault);
+			}
 		});
 	};
 })( jQuery );
