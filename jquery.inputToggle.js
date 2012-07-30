0a5bfeca006c05ee7abc911bd027794e15f58c2f
Jimmy Hillis <jimmy@hillis.me>
2012-07-30 13:24:59 +0800

    Updating for proper placeholder support with HTML5. Always use that element.

--- jquery.inputToggle.js
+++ jquery.inputToggle.js
@@ -5,50 +5,48 @@
 */
 (function( $ ) {
 
-	// Private methods
-	var placeholderSupport
-		, hideDefault
-		, showDefault;
+    // Private methods
+    var placeholderSupport
+        , hideDefault
+        , showDefault;
 
-	// Check for HTML5 form "placeholder" support, a better
-	// solution for the whole problem
-	placeholderSupport = function() {
-		var input_element = document.createElement('input');
-		return ('placeholder' in input_element);
-	}
+    // Check for HTML5 form "placeholder" support, a better
+    // solution for the whole problem
+    placeholderSupport = function() {
+        var input_element = document.createElement('input');
+        return ('placeholder' in input_element);
+    };
 
-	// If the current value matches the default, remove it.
-	hideDefault = function() { 
-		if($(this).val() === $(this).attr('data-initialvalue')) {
-			$(this).val('');
-		}
-	}
-	// If the current input value is empty (space)
-	// then put the default back in
-	showDefault = function() {
-		if(/^(\s)*$/i.test($(this).val())) {
-			$(this).val($(this).attr('data-initialvalue'));
-		}
-	}
+    // If the current value matches the default, remove it.
+    hideDefault = function() {
+        if($(this).val() === $(this).attr('placeholder')) {
+            $(this).val('');
+        }
+    };
 
-	// Create inputToggle public space and allow for
-	// options. Run initial functionality to set the 
-	// default values and bind listeners to events.
-  $.fn.inputToggle = function(options) {
-  	options = options || {};
-		return this.each(function() {
-			if (placeholderSupport()) {
-				$(this).attr('placeholder', $(this).val());
-				$(this).val('');
-			} 
-			else {
-				// Set the initial value to be stored in the elements data-
-				$(this).attr('data-initialvalue', $(this).val());
-				// On focus if the default is set remove it
-				$(this).bind('focus', hideDefault);
-				// On focus if there is no value provided, set it as default
-				$(this).bind('blur', showDefault);
-			}
-		});
-	};
+    // If the current input value is empty (space)
+    // then put the default back in
+    showDefault = function() {
+        if(/^(\s)*$/i.test($(this).val())) {
+            $(this).val($(this).attr('placeholder'));
+        }
+    };
+
+    // Create inputToggle public space and allow for
+    // options. Run initial functionality to set the
+    // default values and bind listeners to events.
+    $.fn.inputToggle = function(options) {
+        options = options || {};
+        return this.each(function() {
+            if (!placeholderSupport()) {
+                // Set the initial value from placeholder
+                $(this).val($(this).attr('placeholder'));
+                // On focus if the default is set remove it
+                $(this).bind('focus', hideDefault);
+                // On focus if there is no value provided, set it as default
+                $(this).bind('blur', showDefault);
+            }
+        });
+    };
+
 })( jQuery );
